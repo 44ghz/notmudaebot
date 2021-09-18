b76dce8b5ed85fd43091ecbb7ab77455a03d8db6
George Perez <perezg3@winthrop.edu>
2021-09-18 16:52:10 -0400

    cl command working properly

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ CHANNEL = os.getenv('CHANNEL')
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='$$', intents = intents)
-members = ["redpalindrome", "wormdisco", "NotMudae"]
+members = []
 text_channel: discord.TextChannel
 
 #### Values for marriage messages
@@ -33,7 +33,7 @@ async def on_ready():
     for guild in bot.guilds:
         if int(guild.id) == int(GUILD):
             global members
-            members = list(m.name for m in guild.members)
+            members = list(m.name for m in filter(lambda member: member.bot == False, guild.members))
         for channel in guild.text_channels:
             if int(channel.id) == int(CHANNEL):
                 global text_channel
@@ -49,15 +49,22 @@ async def get_claims_short(ctx):
 @bot.command(name='claims', help="See who has claims left on this rotation", case_insensitive=True)
 async def get_claims(ctx):
     global text_channel
+    now = datetime.utcnow()
 
     # In UTC, the claim intervals are every 3 hours and the hours at which claims reset are a multiple of 3 (6:23 AM UTC, 9:23 AM UTC, etc)
     # Finding the modulo of that will give us the amount of hours that have passed since the previous rotation
-    difference = datetime.utcnow().time().hour % 3
+    difference = now.time().hour % 3
+
+    # if the difference is 0 but the minutes are less than 23, use the previous interval (subtract 3)
+    if difference == 0 and now.minute < 23:
+        difference = 3
+
     interval_difference = datetime.utcnow() + timedelta(hours = difference * -1)
     window = datetime(interval_difference.year, interval_difference.month, interval_difference.day, interval_difference.hour, 23)
-    
-    messages = await text_channel.history(after = window).flatten()
-    bot_messages = list(filter(lambda m: m.author.bot == False, messages))
+
+    messages = await text_channel.history(limit = 10000, after = window, oldest_first = True).flatten()
+
+    bot_messages = list(filter(lambda m: m.author.bot == True, messages))
     #messages_content = list(([m.content, m.created_at] for m in bot_messages)) uncomment for multiple attributes if needed
     messages_content = list(m.content for m in bot_messages)
     
@@ -66,15 +73,14 @@ async def get_claims(ctx):
 
     for message in messages_content:
         if marriage_message in message:
-            left = get_index(message, " ", 1)
-            right = get_index(message, " ", 2)
+            left = get_index(message, "**", 1) + 2 # Add two for bold markdown
+            right = get_index(message, "**", 2)
             member = message[left:right]
             members_married.append(member.strip())
 
     # Find anyone not in the list
-    #members_with_claims = [m for m in members if m not in set(members_married)]
     members_with_claims = list(set(members) - set(members_married))
-    members_with_claims.sort()
+    members_with_claims = sorted(members_with_claims, key = str.casefold)
 
     s = "\n"
 
