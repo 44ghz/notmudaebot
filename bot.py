fee3699ecbd030748650e38fa578e532490e506c
George Perez <perezg3@winthrop.edu>
2021-09-18 22:05:18 -0400

    Added typing indicator, lowered message history limit

--- bot.py
+++ bot.py
@@ -49,49 +49,50 @@ async def get_claims_short(ctx):
 @bot.command(name='claims', help="See who has claims left on this rotation", case_insensitive=True)
 async def get_claims(ctx):
     global text_channel
-    now = datetime.utcnow()
+    async with text_channel.typing():
+        now = datetime.utcnow()
 
-    # In UTC, the claim intervals are every 3 hours and the hours at which claims reset are a multiple of 3 (6:23 AM UTC, 9:23 AM UTC, etc)
-    # Finding the modulo of that will give us the amount of hours that have passed since the previous rotation
-    difference = now.time().hour % 3
+        # In UTC, the claim intervals are every 3 hours and the hours at which claims reset are a multiple of 3 (6:23 AM UTC, 9:23 AM UTC, etc)
+        # Finding the modulo of that will give us the amount of hours that have passed since the previous rotation
+        difference = now.time().hour % 3
 
-    # if the difference is 0 but the minutes are less than 23, use the previous interval (subtract 3)
-    if difference == 0 and now.minute < 23:
-        difference = 3
+        # if the difference is 0 but the minutes are less than 23, use the previous interval (subtract 3)
+        if difference == 0 and now.minute < 23:
+            difference = 3
 
-    interval_difference = datetime.utcnow() + timedelta(hours = difference * -1)
-    window = datetime(interval_difference.year, interval_difference.month, interval_difference.day, interval_difference.hour, 23)
+        interval_difference = datetime.utcnow() + timedelta(hours = difference * -1)
+        window = datetime(interval_difference.year, interval_difference.month, interval_difference.day, interval_difference.hour, 23)
 
-    messages = await text_channel.history(limit = 10000, after = window, oldest_first = True).flatten()
+        messages = await text_channel.history(limit = 3000, after = window, oldest_first = True).flatten()
 
-    bot_messages = list(filter(lambda m: m.author.bot == True, messages))
-    #messages_content = list(([m.content, m.created_at] for m in bot_messages)) uncomment for multiple attributes if needed
-    messages_content = list(m.content for m in bot_messages)
+        bot_messages = list(filter(lambda m: m.author.bot == True, messages))
+        #messages_content = list(([m.content, m.created_at] for m in bot_messages)) uncomment for multiple attributes if needed
+        messages_content = list(m.content for m in bot_messages)
     
-    members_married = []
-    members_with_claims = []
+        members_married = []
+        members_with_claims = []
 
-    for message in messages_content:
-        if marriage_message in message:
-            left = get_index(message, "**", 1) + 2 # Add two for bold markdown
-            right = get_index(message, "**", 2)
-            member = message[left:right]
-            members_married.append(member.strip())
+        for message in messages_content:
+            if marriage_message in message:
+                left = get_index(message, "**", 1) + 2 # Add two for bold markdown
+                right = get_index(message, "**", 2)
+                member = message[left:right]
+                members_married.append(member.strip())
 
-    # Find anyone not in the list
-    members_with_claims = list(set(members) - set(members_married))
-    members_with_claims = sorted(members_with_claims, key = str.casefold)
+        # Find anyone not in the list
+        members_with_claims = list(set(members) - set(members_married))
+        members_with_claims = sorted(members_with_claims, key = str.casefold)
 
-    s = "\n"
+        s = "\n"
 
-    embed = discord.Embed(color=0x875d5d)
-    embed.title = "People with claims" 
-    embed.description = s.join(members_with_claims)
+        embed = discord.Embed(color=0x875d5d)
+        embed.title = "People with claims" 
+        embed.description = s.join(members_with_claims)
 
-    if len(members_with_claims) > 0:
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Everyone's claimed for this interval!")
+        if len(members_with_claims) > 0:
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Everyone's claimed for this interval!")
 
 
 def get_index(input_string, sub_string, ordinal):
